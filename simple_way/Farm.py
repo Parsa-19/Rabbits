fe9a21bed6f5d4908f31317d72be2bd5355f20ed
Parsa <parsakhalife19@gmail.com>
2023-07-29 12:03:41 +0330

    generate the rabbits only

--- simple_way/Farm.py
+++ simple_way/Farm.py
@@ -3,22 +3,56 @@ import random
 from replit import clear
 
 class Rabbit:
+
+    dot = []
     rab_tobe_count = 0
 
     def __init__(self):
-
-        self.identity = Rabbit.rab_tobe_count
+        self.identity = Rabbit.rab_tobe_count    # .identity attribute tells you the index of specific rabbit you choosed4  # the first time its not actually a rabbit (out_of_range_rab = Rabbit)                                      
         self.character = '   R   '
 
+        self.put() # put every rabbit one time when it's created 
+
         Rabbit.rab_tobe_count += 1
-
-
-
-
-    # def display(self):
         
 
+
+
     # def empty_cells(self, dot):
+    
+
+    def put(self):
+        '''
+        choose random position between rest of empty cells
+        '''
+        if self.identity == 0 :
+            return 0
+        else :
+            line = random.randint(0, len(Rabbit.dot)-1)
+            item = random.randint(0, len(Rabbit.dot[0])-1) 
+
+            Rabbit.dot[line][item] = self.character 
+
+
+
+    def move(self):
+        '''
+        if nothing was in front then move
+        ''' 
+        line = random.randint(0, len(Rabbit.dot))
+        item = random.randint(0, len(Rabbit.dot[0]))
+
+
+
+
+    @staticmethod
+    def create_map():
+        for h in range(6): # 6 should be command (parameter)
+            dot_line = []
+            for w in range(6): # 6 should be command (parameter)
+                dot_line.append('   .   ')
+            Rabbit.dot.append(dot_line)
+        print("the list is successfully created!")
 
 
 
@@ -26,16 +60,10 @@ class Rabbit:
 
 
 
-def create_dot_list(width, height):
-    dot = []
-    for h in range(height):
-        dot_line = []
-        for w in range(width):
-            dot_line.append('   .   ')
-        dot.append(dot_line)
 
-    print("the list is successfully created!")
-    return dot
+
+
+
 
 def print_map(dot): 
     for h in range(len(dot)):
@@ -43,46 +71,28 @@ def print_map(dot):
             print(w, end='')
         print('\n' ,)
 
-# def delete_map(dot):
-#     for h in range(len(dot)):
-#         for w in dot[h]:
-#             print('\r')
-#         print('\n')
     
 
 
-
-
-
-dot = create_dot_list(4, 4)
-rab = Rabbit()
-
-# i = 0
-# while i < 10:
-#     i += 1
-#     print(f"day : {i}")
-
-#     print_map(dot)
-#     dot = rab.move(dot)
-    
-#     time.sleep(1)
+# create a list of dots at first
+out_of_range_rab = Rabbit()
+out_of_range_rab.create_map()
 
 
 total_rab_objs = []
 i = 0
 while i < 10:
     clear()
+    total_rab_objs.append(Rabbit()) # create rabbit and save it in total_rab_objs
 
     i += 1
     print(f"day : {i}")
 
-    total_rab_objs.append(Rabbit()) #rabbit is created in here
     for rab in total_rab_objs:
-        # rab.display() # rabbit is shown in here
-        # rab.move() 
-        pass
+        rab.move() 
+        
     
-    print_map(dot)
+    print_map(Rabbit.dot)
     time.sleep(1)
 
         
--- simple_way/test this.py
+++ simple_way/test this.py
@@ -1,5 +1,23 @@
+class test_class:
+    def __init__(self, name):
+        self.name = name
+        self.put()
 
-print('\r')
-print("kkhhhhhhhhhhhhhhhhhhhkk")
-print('\r')
-print("jksklkksdjfkldsjfklsdjfkldj\rf")
+
+    def put(self):
+        print(f"put {self.name} done! placed")
+
+    def move(self):
+        print(f"    obj {self.name} is moved! thank you!")
+
+
+tes = []
+i = 0
+while i < 4:
+    tes.append(test_class(f"this_{i}"))
+
+
+    for rab in tes:
+        rab.move()
+
+    i += 1
